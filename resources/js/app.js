9d4bd34887d10b2f9f2c41de7ca0e0a171662b99
kimeu254 <jimkimeu254@gmail.com>
2022-09-26 15:52:45 +0300

    added sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,11 @@ import Router from '@/router'
 import store from '@/store'
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp({})
 app.use(Router)
 app.use(store)
+app.use(VueSweetalert2)
 app.mount('#app')
