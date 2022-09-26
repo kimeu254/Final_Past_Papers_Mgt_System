72b1b06b73ac625a36f67ca3549c3360448220c2
kimeu254 <jimkimeu254@gmail.com>
2022-09-26 17:30:54 +0300

    NewUserController fields

--- app/Http/Controllers/NewUsersController.php
+++ app/Http/Controllers/NewUsersController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use \Response;
+use Illuminate\Support\Facades\Auth;
 
 class NewUsersController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $user = User::create([
+            'first_name' => $request->firstname,
+            'last_name' => $request->lastname,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $user;
+    }
 }
